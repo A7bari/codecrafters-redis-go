859e810ef8ed7cb097b09aeac3d17921aab296c7
Ahbari-M <mohammed.ahbari@usmba.ac.ma>
2024-10-17 19:22:59 +0100

    Refactor server.go to use bufio for reading from connection

--- app/server.go
+++ app/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
@@ -18,15 +19,16 @@ func main() {
 		os.Exit(1)
 	}
 
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
-		}
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
 
-		buf := make([]byte, 1024)
-		_, err = conn.Read(buf)
+	red := bufio.NewReader(conn)
+
+	for {
+		_, err := red.ReadString('\n')
 
 		if err != nil {
 			fmt.Println("Error reading from connection: ", err.Error())
@@ -34,6 +36,5 @@ func main() {
 		}
 
 		conn.Write([]byte("+PONG\r\n"))
-
 	}
 }
