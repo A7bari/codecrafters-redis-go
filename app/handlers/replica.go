0e6cb581ac3d08993163b270dcffa63978a9f12e
a7bari <mohammed.ahbari@usmba.ac.ma>
2024-10-21 18:26:17 +0100

    feat: Add support for RDB file transfer during PSYNC

--- app/handlers/replica.go
+++ app/handlers/replica.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/hex"
 	"fmt"
 
 	"github.com/codecrafters-io/redis-starter-go/app/config"
@@ -15,9 +16,22 @@ func psync(params []resp.RESP) []byte {
 	valid := len(params) > 1 && params[0].Bulk == "?" && params[1].Bulk == "-1"
 
 	if valid {
-		msg := fmt.Sprintf("FULLRESYNC %s 0\r\n", config.Get("master_replid"))
-		return resp.String(msg).Marshal()
+		msg := resp.String(
+			fmt.Sprintf("FULLRESYNC %s 0\r\n", config.Get("master_replid")),
+		).Marshal()
+
+		msg = append(msg, getRdbFile()...)
+		return msg
 	}
 
 	return resp.Error("Uncompleted command").Marshal()
+}
+
+func getRdbFile() []byte {
+	file := "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
+	data, err := hex.DecodeString(file)
+	if err != nil {
+		return nil
+	}
+	return data
 }
