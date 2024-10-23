18e408f61f57d69bea0de7968da067e0356febe2
a7bari <mohammed.ahbari@usmba.ac.ma>
2024-10-23 14:18:14 +0100

    Refactor replica.go to use buffered channel for waiting

--- app/handlers/replica.go
+++ app/handlers/replica.go
@@ -47,7 +47,7 @@ func getRdbFile() []byte {
 func wait(params []resp.RESP) []byte {
 	count, _ := strconv.Atoi(params[0].Bulk)
 	timeout, _ := strconv.Atoi(params[1].Bulk)
-	cha := make(chan bool, count)
+	cha := make(chan bool)
 	ack := 0
 	for i := 0; i < len(config.Get().Replicas); i++ {
 		rep := config.Get().Replicas[i]
@@ -69,7 +69,7 @@ func wait(params []resp.RESP) []byte {
 	}
 
 loop:
-	for ack < count {
+	for count > ack {
 		select {
 		case <-cha:
 			ack++
