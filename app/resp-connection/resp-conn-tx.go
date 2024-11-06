31e7b1c5894d2c9d0fa7799929f1d80d3583ecc8
a7bari <mohammed.ahbari@usmba.ac.ma>
2024-11-06 17:49:41 +0100

    refactor: Improve transaction handling in resp-conn-tx.go

--- app/resp-connection/resp-conn-tx.go
+++ app/resp-connection/resp-conn-tx.go
@@ -1,6 +1,9 @@
 package respConnection
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/codecrafters-io/redis-starter-go/app/handlers"
 	"github.com/codecrafters-io/redis-starter-go/app/resp"
 )
@@ -19,8 +22,18 @@ func (client *RespConn) GetTxHandler(command string) handlers.CommandHandler {
 
 func (client *RespConn) Exec(params []resp.RESP) []byte {
 	if client.TxQueue != nil {
+
+		buf := []byte(fmt.Sprintf("*%d\r\n", len(client.TxQueue)))
+
+		for _, agrs := range client.TxQueue {
+			handler := handlers.GetHandler(strings.ToUpper(agrs[0].Bulk))
+			handlerResponse := handler(agrs[1:])
+
+			buf = append(buf, handlerResponse...)
+		}
+
 		client.TxQueue = nil
-		return resp.Array().Marshal()
+		return buf
 	}
 
 	return resp.Error("ERR EXEC without MULTI").Marshal()
